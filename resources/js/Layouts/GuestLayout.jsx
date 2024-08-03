3986bee9b65f7c2e29adc36c8058aff3b8979a84
Raxeev <rajibgrg25@gmail.com>
2024-08-03 23:52:26 +0545

    Design App Landing page

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,18 +1,13 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
-
 export default function Guest({ children }) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                </Link>
-            </div>
+        <>
+            <div className="flex min-h-screen w-full basis-full flex-wrap justify-center overflow-y-auto">
+                <div className="bg-bg-guest-layout-bg relative hidden w-full basis-1/2 items-center justify-center bg-cover bg-right bg-no-repeat xl:flex"></div>
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {children}
+                <div className="flex min-h-screen w-full basis-full items-center justify-center px-4 py-5 md:basis-1/2">
+                    <div className="lg:w-[480px]">{children}</div>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
