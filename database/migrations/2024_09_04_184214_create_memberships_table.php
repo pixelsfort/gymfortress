f3209b80b53009aabd8a1cd5495446c9152e8f88
Earthweb <107529899+Earthwweb@users.noreply.github.com>
2024-09-05 00:54:15 +0545

    model tables created

---
+++ database/migrations/2024_09_04_184214_create_memberships_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('memberships', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+            $table->string('subtitle')->nullable();
+            $table->string('description')->nullable();
+            $table->float('price');
+            $table->float('discount');
+            $table->string('dicounttype');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('memberships');
+    }
+};
