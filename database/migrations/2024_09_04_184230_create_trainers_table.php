c51a79053b5be49e11ac7d24fce4d097b29bd7d1
Earthweb <107529899+Earthwweb@users.noreply.github.com>
2024-09-05 20:28:40 +0545

    added the trainer table

---
+++ database/migrations/2024_09_04_184230_create_trainers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trainers', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('gender')->nullable();
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->date('dob')->nullable();
+            $table->string('street_address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('zip')->nullable();
+            $table->string('country')->nullable();
+            $table->string('password');
+            $table->string('status')->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trainers');
+    }
+};
--- database/migrations/2024_09_04_184239_create_members_table.php
+++ database/migrations/2024_09_04_184239_create_members_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->id();
+            $table->string('firstname');
             $table->timestamps();
         });
     }
